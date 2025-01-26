9f6c4f486e320f47ba9f1a012bf0a665eec5c047
Xavier Fernandes <xavier.fernandes@vwds.pt>
2025-01-26 17:48:40 +0000

    🍒 add a portrait section

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import HomeSection from './components/home-section/home-section.tsx';
+import StickyCard from './components/sticky-card/sticky-card.tsx';
+
+const App = () => {
+    return (
+        <>
+            <HomeSection></HomeSection>
+            <StickyCard></StickyCard>
+        </>
+    );
+};
+
+export default App;
