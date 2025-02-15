9f01bbf90545e55710cc323254eb940a53a0c799
Xavier Fernandes <xavier.fernandes@vwds.pt>
2025-02-15 13:40:58 +0000

    🪼 change the photos being used in the hovering portraits

--- src/pages/home/sections/hero-section.tsx
+++ src/pages/home/sections/hero-section.tsx
@@ -3,22 +3,22 @@ import { Text } from '../../../components/text/text.tsx';
 import { HoverImage } from '../../../components/images/hover-image/hover-image.tsx';
 
 export const HeroSection = () => {
-    // const imageUrls = [
-    //     '/assets/photos/xavier_01.jpg',
-    //     '/assets/photos/xavier_02.jpg',
-    //     '/assets/photos/xavier_03.jpg',
-    //     '/assets/photos/xavier_04.jpg',
-    //     '/assets/photos/xavier_05.jpg',
-    //     '/assets/photos/xavier_06.jpg',
-    //     '/assets/photos/xavier_07.jpg',
-    // ];
     const imageUrls = [
-        'https://images.unsplash.com/photo-1739467516221-e4008797edb4?q=80&w=3097&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        'https://images.unsplash.com/photo-1739465595182-d3ec24f6ebcd?q=80&w=2683&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        'https://images.unsplash.com/photo-1739323006029-2d8452a47df6?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        'https://images.unsplash.com/photo-1739367889589-ca7e21a9128b?q=80&w=3259&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        'https://images.unsplash.com/photo-1739323924885-b68f59e7faca?q=80&w=3088&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        '/assets/photos/xavier_01.jpg',
+        '/assets/photos/xavier_02.jpg',
+        '/assets/photos/xavier_03.jpg',
+        '/assets/photos/xavier_04.jpg',
+        '/assets/photos/xavier_05.jpg',
+        '/assets/photos/xavier_06.jpg',
+        '/assets/photos/xavier_07.jpg',
     ];
+    // const imageUrls = [
+    //     'https://images.unsplash.com/photo-1739467516221-e4008797edb4?q=80&w=3097&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    //     'https://images.unsplash.com/photo-1739465595182-d3ec24f6ebcd?q=80&w=2683&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    //     'https://images.unsplash.com/photo-1739323006029-2d8452a47df6?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    //     'https://images.unsplash.com/photo-1739367889589-ca7e21a9128b?q=80&w=3259&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    //     'https://images.unsplash.com/photo-1739323924885-b68f59e7faca?q=80&w=3088&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    // ];
 
     return (
         <Container
